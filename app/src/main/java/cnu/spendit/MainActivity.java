5b4cc46960877f79c213c4a6806722bb8505a63e
Kyle.Hoobler.15 <kyle.hoobler.15@cnu.edu>
2018-01-31 14:38:58 -0500

    Made Add button have a fucntion that needs more implementation

--- app/src/main/java/cnu/spendit/MainActivity.java
+++ app/src/main/java/cnu/spendit/MainActivity.java
@@ -1,13 +1,18 @@
 package cnu.spendit;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ImageButton add = (ImageButton)findViewById(R.id.add);
+        Button subtract = (Button)findViewById(R.drawable.subtractbutton);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.user_input, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setView(view);
+
+                final EditText input = (EditText) view.findViewById(R.layout.user_input);
+
+                Dialog dialog = builder.create();
+                dialog.show();
+            }
+        });
 
     }
 
